10024623d4b144377123a6ab95f7a9b7d5d23fc8
Sermeus Steven <etu44050@henallux.be>
2024-03-15 09:38:08 +0100

    Add creation of dir

--- decrypt/__init__.py
+++ decrypt/__init__.py
@@ -1,8 +1,10 @@
 from parsing import str_to_bytes
+import os
 
 def decrypt(in_text:bytes, out_file_path:str, key: str):
     key_pad = key * (len(in_text) // len(key)) + key[:len(in_text) % len(key)]
     decrypted_text = ''.join(chr(c ^ k) for c, k in zip(in_text, str_to_bytes(key_pad)))
+    os.makedirs(os.path.dirname(out_file_path), exist_ok=True)
     with open(out_file_path, "w") as f:
         f.write(decrypted_text)
         
--- encrypt/__init__.py
+++ encrypt/__init__.py
@@ -1,8 +1,10 @@
 from .sanitize import sanitize
 from parsing import str_to_bytes, bytes_to_bin
+import os
 def encrypt(in_text:str, out_file_path:str, key: str):
     in_text = sanitize(in_text)
     key_pad = key * (len(in_text) // len(key)) + key[:len(in_text) % len(key)]
     cyphertext = ''.join(chr(ord(p) ^ ord(k)) for p, k in zip(in_text, key_pad))
+    os.makedirs(os.path.dirname(out_file_path), exist_ok=True)
     with open(out_file_path, "w") as f:
         f.write(cyphertext)
